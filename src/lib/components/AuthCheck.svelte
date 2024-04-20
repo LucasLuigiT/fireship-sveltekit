e04c252ab1d759fb8c4dc358412885604e5e13b6
LucasLuigiT <lucas.tremaroli@lego.com>
2024-04-21 00:11:48 +0200

    feat: add AuthCheck component

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { user } from "$lib/firebase";
+  function redirectToLogin() {
+    window.location.href = "/login";
+  }
+</script>
+
+{#if $user}
+  <slot />
+{:else}
+  <p>You must be logged in to view this page.</p>
+  <a href="/login" on:click|preventDefault={redirectToLogin}>Log in</a>
+{/if}
