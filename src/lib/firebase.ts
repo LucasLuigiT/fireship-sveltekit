5c4004a8bfb05aa18f88a7d268132a9a22f9aa03
LucasLuigiT <lucas.tremaroli@lego.com>
2024-06-16 16:42:18 +0200

    feat: photo upload

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -22,7 +22,7 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storeage = getStorage(app);
+export const storage = getStorage(app);
 
 function userStore() {
     let unsubscribe: () => void;
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-  import { auth, db, user, userData } from "$lib/firebase";
+  import { auth, db, user, userData, storage } from "$lib/firebase";
   import { Stepper, Step } from "@skeletonlabs/skeleton";
   import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
-  import { doc, getDoc, writeBatch } from "firebase/firestore";
+  import { doc, getDoc, writeBatch, updateDoc } from "firebase/firestore";
+  import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
   let isUserLoggedIn = false;
   $: isUserLoggedIn = $user ? true : false;
@@ -16,6 +17,10 @@
   let loading = false;
   let isAvailable = false;
   let debounceTimer: NodeJS.Timeout;
+
+  let previewURL: string;
+  let uploading = false;
+  $: href = `/${$userData?.username}/edit`;
 
   async function signInWithGoogle() {
     const provider = new GoogleAuthProvider();
@@ -57,6 +62,18 @@
     username = "";
     isAvailable = false;
   }
+
+  async function upload(e: any) {
+    uploading = true;
+    const file = e.target.files[0];
+    previewURL = URL.createObjectURL(file);
+    const storageRef = ref(storage, `users/${$user!.uid}/profile.png`);
+    const result = await uploadBytes(storageRef, file);
+    const url = await getDownloadURL(result.ref);
+
+    await updateDoc(doc(db, "users", $user!.uid), { photoURL: url });
+    uploading = false;
+  }
 </script>
 
 <div class="flex justify-center items-center h-screen">
@@ -81,7 +98,7 @@
       {/if}
     </Step>
     <Step>
-      <svelte:fragment slot="header">Username</svelte:fragment>  
+      <svelte:fragment slot="header">Username</svelte:fragment>
       {#if $userData?.username}
         <p>
           You already have a username: <strong>@{$userData.username}</strong>
@@ -128,7 +145,30 @@
     </Step>
     <Step>
       <svelte:fragment slot="header">Photo</svelte:fragment>
-      (content)
+      <form class="max-w-screen-md w-full">
+        <div class="form-control w-full max-w-xs my-10 mx-auto text-center">
+          <img
+            src={previewURL ?? $userData?.photoURL ?? "/user.png"}
+            alt="photoURL"
+            width="256"
+            height="256"
+            class="mx-auto"
+          />
+          <label for="photoURL" class="label">
+            <span class="label-text">Pick a file</span>
+          </label>
+          <input
+            on:change={upload}
+            name="photoURL"
+            type="file"
+            class="file-input file-input-bordered w-full max-w-xs"
+            accept="image/png, image/jpeg, image/gif, image/webp"
+          />
+          {#if uploading}
+            <p>Uploading...</p>
+            <progress class="progress progress-info w-56 mt-6" />
+          {/if}
+        </div>
     </Step>
   </Stepper>
 </div>
